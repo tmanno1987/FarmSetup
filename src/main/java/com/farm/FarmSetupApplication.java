8cb203dc7deaf1b5479ddac524f2a190e5ee2033
Timothy Manno <38054853+tmanno1987@users.noreply.github.com>
2020-11-14 08:57:07 -0600

    initial setup
    backend initial setup for farm app

---
+++ src/main/java/com/farm/FarmSetupApplication.java
@@ -0,0 +1,13 @@
+package com.farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmSetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farm/FarmSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
